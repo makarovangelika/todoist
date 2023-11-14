a62e92494bbb0d4273267e9e939fcf170c420269
Макарова Анжелика <esipovaangelika@mail.ru>
2023-11-14 11:11:00 +0300

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { TasksPageComponent } from './components/tasks-page/tasks-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'signup', component: SignUpPageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'tasks', component: TasksPageComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpPageComponent,
+    LoginPageComponent,
+    TasksPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
