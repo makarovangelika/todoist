daf1d323d18555a1962105ca111fc9f06d556c07
Макарова Анжелика <esipovaangelika@mail.ru>
2023-11-15 13:40:27 +0300

    Service for storage of users in local storage

---
+++ src/app/services/user-storage.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserStorageService {
+
+  constructor() { }
+
+  getUser(email: string): User | null {
+    const item = localStorage.getItem(email);
+    if (item === null) {
+      return null;
+    }
+    return JSON.parse(item);
+  }
+
+  addUser(user: User) {
+    const item = JSON.stringify(user);
+    localStorage.setItem(user.email, item);
+  }
+}
