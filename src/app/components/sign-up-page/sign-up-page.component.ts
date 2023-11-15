9b30a5186ebce8019084402d776bfa57732643b4
Макарова Анжелика <esipovaangelika@mail.ru>
2023-11-15 17:03:22 +0300

    Add event listener for registration form

--- src/app/components/sign-up-page/sign-up-page.component.ts
+++ src/app/components/sign-up-page/sign-up-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models';
+import { SignUpService } from 'src/app/services/sign-up.service';
 
 @Component({
   selector: 'app-sign-up-page',
@@ -7,10 +10,23 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./sign-up-page.component.scss']
 })
 export class SignUpPageComponent {
+
   registrationForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.pattern('(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}')]]
   })
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder,
+              private signUpService: SignUpService,
+              private router: Router) {}
+
+  signUp() {
+    const signupSucess = this.signUpService.registerUser({
+      email: this.registrationForm.value.email as string,
+      password: this.registrationForm.value.password as string,
+    });
+    if (signupSucess) {
+      this.router.navigate(['/tasks']);
+    }
+  }
 }
