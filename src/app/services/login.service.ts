7beeea6471c73ffee2c90e8255b8881704f8c793
Макарова Анжелика <esipovaangelika@mail.ru>
2023-11-16 11:23:37 +0300

    Create login service

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { UserStorageService } from './user-storage.service';
+import { User } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private userStorageService: UserStorageService) { }
+
+  checkUser(user: User) {
+    const registeredUser = this.userStorageService.getUser(user.email);
+    if (registeredUser && (user.password === registeredUser.password)) {
+      return true;
+    }
+    return false;
+  }
+}
