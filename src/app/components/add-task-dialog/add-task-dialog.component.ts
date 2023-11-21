128e11d17efbdcfe614a2feb80fef8432b925023
Макарова Анжелика <esipovaangelika@mail.ru>
2023-11-21 15:40:52 +0300

    Create handler for addtask button in dialog window

--- src/app/components/add-task-dialog/add-task-dialog.component.ts
+++ src/app/components/add-task-dialog/add-task-dialog.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { Task } from 'src/app/models';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-add-task-dialog',
@@ -9,7 +12,17 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class AddTaskDialogComponent {
   addTaskForm = this.fb.group({
     description: ['', Validators.required]
-  })
+  });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder,
+              private taskService: TaskService,
+              public ref: DynamicDialogRef) {}
+
+  addTask() {
+    const task = {
+      description: this.addTaskForm.value.description
+    } as Task;
+    this.taskService.addTask(task);
+    this.ref.close();
+  }
 }
