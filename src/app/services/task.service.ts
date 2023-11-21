ef1c90ce35b29121952c48fc139942894ab96dfa
Макарова Анжелика <esipovaangelika@mail.ru>
2023-11-21 12:26:15 +0300

    Change service to store tasks in separate item Create addtask method

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@angular/core';
 import { UserStorageService } from './user-storage.service';
+import { Task } from '../models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  tasks = [
-    {done: false, description: 'Приготовить'},
-    {done: false, description: 'Убраться'},
-    {done: false, description: 'Работать'}
-]
+  authorizedUser = this.userStorageService.getAuthorizedUser();
 
   constructor(private userStorageService: UserStorageService) { }
 
-  getTasks() {
-    const authorizedUser = this.userStorageService.getAuthorizedUser();
-    const tasks = this.userStorageService.getUser(authorizedUser.email)?.tasks;
+  getTasks(): Task[] {
+    const tasks = localStorage.getItem(`tasks: ${this.authorizedUser.email}`);
     if (!tasks) {
       return [];
     }
-    return tasks;
+    return JSON.parse(tasks);
+  }
+  addTask(task: Task) {
+    const tasks = this.getTasks();
+    tasks.push(task);
+    const serializedTasks = JSON.stringify(tasks);
+    localStorage.setItem(`tasks: ${this.authorizedUser.email}`, serializedTasks);
   }
 }
