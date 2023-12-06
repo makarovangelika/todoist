cc37e2346e7c0d0d532856d8799e2740a4d4dfd2
Макарова Анжелика <esipovaangelika@mail.ru>
2023-12-06 16:28:44 +0300

    Set translations to russian

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import * as translations from '../../ru.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todoist';
 
-  constructor() {}
+  constructor(private config: PrimeNGConfig) {}
 
-
+  ngOnInit() {
+    this.config.setTranslation(translations);
+  }
 }
