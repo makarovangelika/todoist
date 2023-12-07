6125a6282357373044c5157731376460605e715f
Макарова Анжелика <esipovaangelika@mail.ru>
2023-12-07 12:38:26 +0300

    Create filter component, add styles

---
+++ src/app/components/filter/filter.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, WritableSignal } from '@angular/core';
+import { FilterService } from 'src/app/services/filter.service';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.scss']
+})
+export class FilterComponent {
+  constructor(public filterService: FilterService) {}
+}
