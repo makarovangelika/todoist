b69de65eda03b9144f488ea5189d943b5d019851
Макарова Анжелика <esipovaangelika@mail.ru>
2023-12-15 19:21:05 +0300

    Delete unused import

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PRIORITY_LABELS } from 'src/app/constants';
 import { Task } from 'src/app/models';
 
